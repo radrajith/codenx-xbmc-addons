2c6def0bdc7ad4f2e20b63017ba80b2db98c6290
radrajith <radrajith@gmail.com>
2013-04-17 19:23:51 -0300

    Update default.py

--- plugin.video.oliyumoliyum/default.py
+++ plugin.video.oliyumoliyum/default.py
@@ -713,7 +713,7 @@ def Load_and_Play_Video_Links(url,name):
    return ok
 
 def Main_Categories():
-   url = "http://www.dishtamilonline.com/"
+   url = "http://www.motiontamil.com/"
    req = urllib2.Request(url)
    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
    response = urllib2.urlopen(req)
